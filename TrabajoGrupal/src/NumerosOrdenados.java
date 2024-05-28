c7935e885609682c449ab8fbf68e598e6eda7b93
Natasha Nicole Navarro Nolasco <nicolenolas24@gmail.com>
2024-05-28 16:42:39 -0600

    Natasha

--- TrabajoGrupal/src/NumerosOrdenados.java
+++ TrabajoGrupal/src/NumerosOrdenados.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+import java.util.TreeSet;
+
 public class NumerosOrdenados {
     public static void main(String[] args) throws Exception {
-      
+        Scanner scanner = new Scanner(System.in);
+        TreeSet<Integer> numeros = new TreeSet<>();
+
+        System.out.println("Ingrese números. Escriba 'salir' para terminar.");
     }
     
 }
