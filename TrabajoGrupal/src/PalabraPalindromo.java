4eb3f221d4dea542ccef470fc38abb642052a2ab
Ingrid Jacqueline Gabriel Zepeda <jacquelinegabriel05@hotmail.com>
2024-05-29 21:51:47 -0600

    Ingrid

--- TrabajoGrupal/src/PalabraPalindromo.java
+++ TrabajoGrupal/src/PalabraPalindromo.java
@@ -1,9 +1,22 @@
-public class PalabraPalindromo {
-    public static boolean esPalindromo(String palabra) {
-        String Limpiar = palabra.replaceAll("[^a-zA-Z]", "").toLowerCase();
-        String Regresar = new StringBuilder(Limpiar).reverse().toString();
-        return Limpiar.equals(Regresar);
+import java.util.Scanner;
 
-        //mm
+public class PalabraPalindromo {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingresa una palabra : ");
+        String palabraI = scanner.nextLine();
+
+        if (esPalindromo(palabraI)) {
+            System.out.println("La palabra ingresada es un palíndromo. ");
+        } else {
+            System.out.println("La palabra ingresada no es un palíndromo. ");
+        }
+    }
+
+    public static boolean esPalindromo(String palabra) {
+        String limpiar = palabra.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        String regresar = new StringBuilder(limpiar).reverse().toString();
+        return limpiar.equals(regresar);
     }
 }
